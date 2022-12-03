58b6d260f6ba85d78ee6e35343adf73c38f8f2ed
Eslam G. Khoga <KhogaEslam@gmail.com>
2022-12-03 17:40:54 +0200

    Writing unit tests for GildedRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,11 +7,26 @@ class GildedRose {
         this.items = items;
     }
 
+    /***
+     - Once the sell by date has passed, Quality degrades twice as fast
+     - The Quality of an item is never negative [quality > 0]
+     - "Aged Brie" actually increases in Quality the older it gets ["Aged Brie" quality +]
+     - The Quality of an item is never more than 50 [quality <= 50]
+     - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+     - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+     quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
+     but quality drops to 0 after the concert
+     * Just for clarification, an item can never have its Quality increase above 50,
+     * however "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+     */
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
                 if (items[i].quality > 0) {
+
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
@@ -21,13 +36,16 @@ class GildedRose {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
                         if (items[i].sellIn < 11) {
+
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
+
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
@@ -41,9 +59,13 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
+
                 if (!items[i].name.equals("Aged Brie")) {
+
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
                         if (items[i].quality > 0) {
+
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,176 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void testThrowsNPE() {
+        GildedRose app = new GildedRose(null);
+        try {
+            app.updateQuality();
+        } catch (NullPointerException e) {
+            assertTrue(true);
+        }
     }
 
+    @Test
+    public void testEmptyItems() {
+        Item[] items = new Item[] {};
+        GildedRose app = new GildedRose(items);
+        assertEquals(0, app.items.length);
+    }
+
+    @Test
+    void testDecreaseQualityByOneWhereQualityGreaterThanZero_OtherItems() {
+        Item[] items = new Item[] {
+                new Item("foo", 1, 0),
+                new Item("foo", 1, 1),
+                new Item("foo", 1, 2),
+                new Item("foo", 1, 3)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[1].quality);
+        assertEquals(0, app.items[1].sellIn);
+        assertEquals(1, app.items[2].quality);
+        assertEquals(0, app.items[2].sellIn);
+        assertEquals(2, app.items[3].quality);
+        assertEquals(0, app.items[3].sellIn);
+    }
+
+    @Test
+    void testSellInPassedDecreaseQualityByTwoWhereQualityGreaterThanZero_OtherItems() {
+        Item[] items = new Item[] {
+                new Item("foo", 0, 0),
+                new Item("foo", 0, 1),
+                new Item("foo", 0, 2),
+                new Item("foo", 0, 3),
+                new Item("foo", 0, 4)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
+        assertEquals(0, app.items[2].quality);
+        assertEquals(-1, app.items[2].sellIn);
+        assertEquals(1, app.items[3].quality);
+        assertEquals(-1, app.items[3].sellIn);
+        assertEquals(2, app.items[4].quality);
+        assertEquals(-1, app.items[4].sellIn);
+    }
+
+    @Test
+    void testIncreaseQualityByOneWhereQualityLessThanFifty_AgedBrie() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 1, 45),
+                new Item("Aged Brie", 1, 49),
+                new Item("Aged Brie", 1, 50),
+                new Item("Aged Brie", 1, 0)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(46, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(50, app.items[1].quality);
+        assertEquals(0, app.items[1].sellIn);
+        assertEquals(50, app.items[2].quality);
+        assertEquals(0, app.items[2].sellIn);
+        assertEquals(1, app.items[3].quality);
+        assertEquals(0, app.items[3].sellIn);
+    }
+
+    @Test
+    void testSellInPassedIncreaseQualityByTwoWhereQualityLessThanFifty_AgedBrie() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 0, 45),
+                new Item("Aged Brie", 0, 49),
+                new Item("Aged Brie", 0, 50),
+                new Item("Aged Brie", 0, 0)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(47, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(50, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
+        assertEquals(50, app.items[2].quality);
+        assertEquals(-1, app.items[2].sellIn);
+        assertEquals(2, app.items[3].quality);
+        assertEquals(-1, app.items[3].sellIn);
+    }
+
+    @Test
+    void testIncreaseQualityByTwoWhereQualityLessThanFiftyAndSellInLessThanEleven_Backstage() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 45),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(47, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(50, app.items[1].quality);
+        assertEquals(9, app.items[1].sellIn);
+        assertEquals(50, app.items[2].quality);
+        assertEquals(9, app.items[2].sellIn);
+        assertEquals(50, app.items[3].quality);
+        assertEquals(9, app.items[3].sellIn);
+        assertEquals(2, app.items[4].quality);
+        assertEquals(9, app.items[4].sellIn);
+    }
+
+    @Test
+    void testIncreaseQualityByThreeWhereQualityLessThanFiftyAndSellInLessThanSix_Backstage() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 45),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 48),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 0)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(48, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(50, app.items[1].quality);
+        assertEquals(0, app.items[1].sellIn);
+        assertEquals(50, app.items[2].quality);
+        assertEquals(0, app.items[2].sellIn);
+        assertEquals(50, app.items[3].quality);
+        assertEquals(0, app.items[3].sellIn);
+        assertEquals(3, app.items[4].quality);
+        assertEquals(0, app.items[4].sellIn);
+    }
+
+    @Test
+    void testSellInPassedResetQualityToZeroWhereQualityLessThanFifty_Backstage() {
+        Item[] items = new Item[] {
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 45),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 48),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
+        assertEquals(0, app.items[2].quality);
+        assertEquals(-1, app.items[2].sellIn);
+        assertEquals(0, app.items[3].quality);
+        assertEquals(-1, app.items[3].sellIn);
+        assertEquals(0, app.items[4].quality);
+        assertEquals(-1, app.items[4].sellIn);
+    }
 }
